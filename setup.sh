5a0c988be2a96d9a0277fb4ee50a7e9e4db8368c
Colin Constable <colin@constable.cc>
2024-02-24 21:17:20 -0800

    fix: more bugs

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ cp catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
 #rm -rf /tmp/catppuccin
 #
 # get ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 # get zsh syntax highlighter
 zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+# get ready to install
+rm ~/.zshrc
+./install
