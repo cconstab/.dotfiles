e4d6f219f75ad5ffc3b36a5d0bf53d9d25817a83
Colin Constable <colin@constable.cc>
2024-02-24 21:06:14 -0800

    fix :order

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 mkdir ~/.zsh
-# get zsh syntax highlighter
-zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 # get catppuccin
 git clone https://github.com/catppuccin/zsh-syntax-highlighting.git /tmp/catppuccin
 cd /tmp/catppuccin/themes/
@@ -10,3 +8,5 @@ cp catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
 #
 # get ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# get zsh syntax highlighter
+zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
