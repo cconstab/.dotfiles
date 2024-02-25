b6829a501a58463c68a952a0614905807e63965c
Colin Constable <colin@constable.cc>
2024-02-24 21:28:05 -0800

    fix: more bugs

--- setup.sh
+++ setup.sh
@@ -11,5 +11,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # get zsh syntax highlighter
 zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 # get ready to install
+cd -
 rm ~/.zshrc
 ./install
